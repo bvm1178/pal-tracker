30d5dfbfae80c441e97ce4b7ddd62e0ee99be53c
bvm1178 <bvm1178@gmail.com>
2017-07-21 23:54:18 +0300

    Cloud Foundry Services & Database migrations lab changes

--- src/main/java/io/pivotal/pal/tracker/TimeEntriesController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntriesController.java
@@ -2,8 +2,6 @@ package io.pivotal.pal.tracker;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
